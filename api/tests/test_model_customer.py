120c114e6f60c5d47b1b5a81a934ed0e5e6e78a5
eles <eleswastaken@gmail.com>
2022-12-03 08:21:56 +0300

    Write tests for customer model

---
+++ api/tests/test_model_customer.py
@@ -0,0 +1,100 @@
+from django.test import TestCase
+
+from api.models import Customer
+
+class TestingCustomerModel(TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_customer_creation(self):
+        
+        customer_n_before = Customer.objects.count()
+
+        self.assertEqual(customer_n_before, 0)
+
+        customer = Customer.objects.create(
+                name  = 'Customer Name',
+                phone = '+123 456 789 10',
+                email = 'customer@gmail.com',
+                )
+
+        customer_n_after = Customer.objects.count()
+
+        self.assertEqual(customer_n_after, 1)
+
+
+    def test_has_api_key(self):
+
+        # create user
+        customer_n_before = Customer.objects.count()
+        self.assertEqual(customer_n_before, 0)
+        customer = Customer.objects.create(
+                name  = 'Customer Name',
+                phone = '+123 456 789 10',
+                email = 'customer@gmail.com',
+                )
+        customer_n_after = Customer.objects.count()
+        self.assertEqual(customer_n_after, 1)
+
+        #test
+        self.assertTrue(hasattr(customer, 'api_key'))
+
+
+    def test_changing_api_key_on_update(self):
+
+        # create user
+        customer_n_before = Customer.objects.count()
+        self.assertEqual(customer_n_before, 0)
+        customer = Customer.objects.create(
+                name  = 'Customer Name',
+                phone = '+123 456 789 10',
+                email = 'customer@gmail.com',
+                )
+        customer_n_after = Customer.objects.count()
+        self.assertEqual(customer_n_after, 1)
+
+        self.assertTrue(hasattr(customer, 'api_key'))
+
+        #test
+        old_api_key = customer.api_key
+
+        customer.name = 'New Name'
+        customer.save()
+
+        new_api_key = customer.api_key
+
+        self.assertNotEqual(old_api_key, new_api_key)
+
+
+    def test_customer_deletion(self):
+        # create user
+        customer_n_before = Customer.objects.count()
+        self.assertEqual(customer_n_before, 0)
+        customer = Customer.objects.create(
+                name  = 'Customer Name',
+                phone = '+123 456 789 10',
+                email = 'customer@gmail.com',
+                )
+        customer_n_after = Customer.objects.count()
+        self.assertEqual(customer_n_after, 1)
+
+        customer.delete()
+
+        #test
+        customer_n_after = Customer.objects.count()
+        self.assertEqual(customer_n_after, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- api/tests/test_testing.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class Testing_folders(TestCase):
-
-    def test_folder(self):
-
-        self.assertEqual(True, True)
