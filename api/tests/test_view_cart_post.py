b045680dbb895c1d92c3411a1c83ebbfbaca220a
eles <eleswastaken@gmail.com>
2022-12-11 20:30:23 +0300

    Create cart items view POST

---
+++ api/tests/test_view_cart_post.py
@@ -0,0 +1,103 @@
+from django.test import TestCase, Client
+
+from rest_framework import status
+
+from api.models import Customer, Product, Brand, Category, CartItem
+
+class TestViewCart(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+        self.customer = Customer.objects.create(
+                name  = 'Customer Name',
+                phone = '+123 456 789 10',
+                email = 'customer@gmail.com',
+                password = '1234',
+                )
+        
+        self.category = Category.objects.create(
+                name  = 'Parent',
+                description = 'This is the parent category.',
+                )
+
+        self.brand = Brand.objects.create(
+                name  = 'Poma',
+                description = 'This is the parent category.',
+                )
+
+        self.product = Product.objects.create(
+                name = 'Test Product 1',
+                description = 'This is the description of the product.',
+                category = self.category,
+                brand = self.brand,
+                in_stock_amount = 12,
+                sold_amount = 12,
+                price = 1234,
+                )
+
+    def test_getting_data(self):
+
+        quantity = 2
+
+        response = self.c.post(
+                '/api/cart/',
+                {
+                    "product_id": self.product.id,
+                    "quantity": quantity,
+                    },
+                HTTP_AUTHORIZATION='API-KEY ' + self.customer.api_key,
+                )
+
+        # print(response.data)
+
+        item = CartItem.objects.get(customer=self.customer)
+
+        self.assertIsNotNone(item)
+
+        self.assertEqual(item.product.id, self.product.id)
+
+        self.assertEqual(item.quantity, quantity)
+
+
+    def test_400_BAD_REQUEST(self):
+
+        response = self.c.post(
+                '/api/cart/',
+                {
+                    "product_id": self.product.id,
+                    # "quantity": 1,
+                    },
+                HTTP_AUTHORIZATION='API-KEY ' + self.customer.api_key,
+                )
+
+        # print(response.data)
+
+        self.assertEqual(
+                response.status_code,
+                status.HTTP_400_BAD_REQUEST
+                )
+
+
+    def test_404_NOT_FOUND_product(self):
+
+        response = self.c.post(
+                '/api/cart/',
+                {
+                    "product_id": 123451234, # WRONG ID
+                    "quantity": 1,
+                    },
+                HTTP_AUTHORIZATION='API-KEY ' + self.customer.api_key,
+                )
+
+        # print(response.data)
+
+        self.assertEqual(
+                response.status_code,
+                status.HTTP_404_NOT_FOUND,
+                )
+
+
+
+
+
--- api/views/cart.py
+++ api/views/cart.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from api.models import CartItem
+from api.models import CartItem, Product
 from api.serializers import CartItemSerializer
 
 class CartView(APIView):
@@ -24,6 +24,43 @@ class CartView(APIView):
                     'cart_items': serializer.data,
                     },
                 )
+
+    def post(self, request):
+
+        quantity = request.data.get('quantity')
+        product_id = request.data.get('product_id')
+
+        if not quantity or not product_id:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            item = CartItem.objects.create(
+                        customer=request.customer,
+                        product=product,
+                        quantity=quantity,
+                    )
+
+            serializer = CartItemSerializer(item)
+
+            return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                    )
+
+        except Exception:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        
+
+
+
+
+
 
 
 
